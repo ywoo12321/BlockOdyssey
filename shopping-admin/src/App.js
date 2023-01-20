fa4167d13a60b6c3198ee339d8ae0d5c447c1078
ywoo12321 <ywoo12321@gmail.com>
2023-01-20 17:57:03 +0900

    feat: section 구분 및 css 설정 select에 사용할 함수 미리 설정

--- shopping-admin/src/App.js
+++ shopping-admin/src/App.js
@@ -1,11 +1,22 @@
-function App() {
+import React, { useState } from "react";
+import "./App.css";
+const App = (props) => {
+  const selectList = ["전체", "상품명", "브랜드", "상품내용"];
+  const [Selected, setSelected] = useState("");
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <div>BlockOdyssey 과제 테스트 초기 세팅</div>
-      </header>
+      <section className="top"></section>
+      <section className="middle">
+        <div className="numberData">
+          <p>검색된 데이터 : {"100"}건</p>
+        </div>
+      </section>
+      <section className="bottom"></section>
     </div>
   );
-}
+};
 
 export default App;
