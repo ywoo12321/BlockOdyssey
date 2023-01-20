359790eb346e9547002517058b652f4e9d8165d1
ywoo12321 <ywoo12321@gmail.com>
2023-01-20 15:41:55 +0900

    setting: 과제테스트 초기 react setting

---
+++ shopping-admin/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>BlockOdyssey 과제 테스트 초기 세팅</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
